939c6f857def12a2536d456b02f5b87f307fba6b
zryzhangruyi <731134543@qq.com>
2016-10-02 15:13:54 +0800

    Initial commit

---
+++ mobile/src/main/java/com/winorout/followme/FollowActivity.java
@@ -0,0 +1,13 @@
+package com.winorout.followme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FollowActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_follow);
+    }
+}
