bd3f37680dbe149eb8565f38a6d4ed0fa2586b75
zryzhangruyi <731134543@qq.com>
2016-10-04 20:48:59 +0800

    删除透明标题栏

---
+++ mobile/src/main/java/com/winorout/app/FollowApplication.java
@@ -0,0 +1,16 @@
+package com.winorout.app;
+
+import android.app.Application;
+
+/**
+ *
+ * @author  ryzhang
+ * @data 2016/10/4 20:47
+ */
+
+public class FollowApplication extends Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
