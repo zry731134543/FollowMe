1eeb667349b23007dc13b68fdfb2e59035b7950c
shijun931011 <2301855349@qq.com>
2016-10-23 12:00:54 +0800

    语音识别

--- wear/src/main/java/com/winorout/followme/BarrageActivity.java
+++ wear/src/main/java/com/winorout/followme/BarrageActivity.java
@@ -1,6 +1,10 @@
 package com.winorout.followme;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.winorout.base.BaseActivity;
 
@@ -9,15 +13,22 @@ import com.winorout.base.BaseActivity;
  */
 
 public class BarrageActivity extends BaseActivity {
+    private Button mStartVoiceBtn;
+    private TextView mVoiceTv;
+
     @Override
     public void initVariables() {
-
+        Intent voiceIntent = new Intent(this, VoiceInputActivity.class);
+        voiceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(voiceIntent);
     }
 
     @Override
     public void initView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_barrage);
         initTopbar("弹幕");
+        mStartVoiceBtn = (Button) findViewById(R.id.test_button);
+        mVoiceTv = (TextView) findViewById(R.id.speak_tip);
     }
 
     @Override
