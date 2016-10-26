f448dcf17a1510953413530c19f6f252db982c53
Little7 <yan956462326@gmail.com>
2016-10-26 20:42:32 +0800

    添加我的设备功能

---
+++ mobile/src/main/java/com/winorout/followme/personalCenter/DeviceInfo.java
@@ -0,0 +1,34 @@
+package com.winorout.followme.personalCenter;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.winorout.followme.R;
+
+public class DeviceInfo extends Activity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_info);
+        webView = (WebView) findViewById(R.id.webView);
+        WebSettings webSettings =webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);  //支持js
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        webView.loadUrl("file:///android_asset/video.html");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        webView.destroy();
+    }
+}
--- mobile/src/main/java/com/winorout/followme/personalCenter/PersonalCenterFragment.java
+++ mobile/src/main/java/com/winorout/followme/personalCenter/PersonalCenterFragment.java
@@ -1,26 +1,54 @@
 package com.winorout.followme.personalCenter;
 
 
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.mobvoi.android.common.ConnectionResult;
+import com.mobvoi.android.common.api.MobvoiApiClient;
+import com.mobvoi.android.common.api.ResultCallback;
+import com.mobvoi.android.wearable.MessageApi;
+import com.mobvoi.android.wearable.MessageEvent;
+import com.mobvoi.android.wearable.Wearable;
 import com.winorout.followme.R;
+
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 /**
  * Created by Mr-x on 2016/10/03.
  */
 
-public class PersonalCenterFragment extends Fragment implements View.OnClickListener {
+public class PersonalCenterFragment extends Fragment implements View.OnClickListener, MessageApi.MessageListener, MobvoiApiClient.ConnectionCallbacks, MobvoiApiClient.OnConnectionFailedListener {
 
+    private static final String TAG = "qmyan";
     private View view;
     private LinearLayout exerciseLayout;
     private LinearLayout goalsLayout;
-    private LinearLayout HistoryLayout;
+    private LinearLayout historyLayout;
+    private LinearLayout deviceLayout;
+
+    private ImageView deviceImg;
+    private TextView deviceInfo;
+
+    private boolean isConnected;
+    private MobvoiApiClient mClient;
+    private Handler handler;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -37,19 +65,65 @@ public class PersonalCenterFragment extends Fragment implements View.OnClickList
     private void init() {
         exerciseLayout=(LinearLayout) view.findViewById(R.id.exercise);
         goalsLayout=(LinearLayout) view.findViewById(R.id.goals);
-        HistoryLayout=(LinearLayout) view.findViewById(R.id.History);
+        historyLayout=(LinearLayout) view.findViewById(R.id.history);
+        deviceLayout=(LinearLayout) view.findViewById(R.id.device);
 //		settingLayout=(LinearLayout) view.findViewById(R.id.setting);
 
         exerciseLayout.setOnClickListener(this);
         goalsLayout.setOnClickListener(this);
-        HistoryLayout.setOnClickListener(this);
+        historyLayout.setOnClickListener(this);
+        deviceLayout.setOnClickListener(this);
 //		settingLayout.setOnClickListener(this);
+
+        deviceImg = (ImageView) view.findViewById(R.id.deviceImg);
+        deviceInfo = (TextView) view.findViewById(R.id.deviceInfo);
+
+        //调用API
+        mClient = new MobvoiApiClient.Builder(getContext())
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(Wearable.API)
+                .build();
+
+        handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                if (msg.what == 0x123) {
+                }
+                if (msg.what == 0x124) {
+                    //检查设备是否连接
+                    checkConnectState();
+                    //设置我的设备显示信息
+                    initMyDevice();
+                }
+                if (msg.what == 0x125) {
+                }
+            }
+        };
+
+        //启动一个新的线程定时更新状态
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Message msg = new Message();
+                msg.what = 0x124;
+                handler.sendMessage(msg);
+            }
+        }, 0, 300);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        //连接API
+        mClient.connect();
+    }
+
     @Override
     public void onClick(View v) {
         switch(v.getId())
         {
-            case R.id.History:
+            case R.id.history:
                 Intent intent = new Intent(getActivity(), Historcal.class);
                 startActivity(intent);
                 break;
@@ -61,8 +135,121 @@ public class PersonalCenterFragment extends Fragment implements View.OnClickList
                 Intent intent2 = new Intent(getActivity(),Goals.class);
                 startActivity(intent2);
                 break;
+            case R.id.device:
+                if (isConnected) {
+                    //如果已经和手表连接设备,打开设备信息界面
+                    Intent intent3 = new Intent(getActivity(), DeviceInfo.class);
+                    startActivity(intent3);
+                } else {
+                    //如果已经和手表连接设备,打开TicWear助手连接设备界面
+                    doStartApplicationWithPackageName("com.mobvoi.companion");
+                }
+                break;
+        }
+    }
+
+    // 通过包名获取此APP详细信息，包括Activities、services、versioncode、name等等
+    private void doStartApplicationWithPackageName(String packagename) {
+        PackageInfo packageinfo = null;
+        try {
+            packageinfo = getActivity().getPackageManager().getPackageInfo(packagename, 0);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        if (packageinfo == null) {
+            return;
         }
 
+        // 创建一个类别为CATEGORY_LAUNCHER的该包名的Intent
+        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+        resolveIntent.setPackage(packageinfo.packageName);
 
+        // 通过getPackageManager()的queryIntentActivities方法遍历
+        List<ResolveInfo> resolveinfoList = getActivity().getPackageManager()
+                .queryIntentActivities(resolveIntent, 0);
+
+        ResolveInfo resolveinfo = resolveinfoList.iterator().next();
+        if (resolveinfo != null) {
+            // packagename = 参数packname
+            String packageName = resolveinfo.activityInfo.packageName;
+            // 这个就是我们要找的该APP的LAUNCHER的Activity[组织形式：packagename.mainActivityname]
+            String className = resolveinfo.activityInfo.name;
+            // LAUNCHER Intent
+            Intent intent = new Intent(Intent.ACTION_MAIN);
+            intent.addCategory(Intent.CATEGORY_LAUNCHER);
+
+            // 设置ComponentName参数1:packagename参数2:MainActivity路径
+            ComponentName cn = new ComponentName(packageName, className);
+
+            intent.setComponent(cn);
+            startActivity(intent);
+        }
     }
+
+    //更新我的设备显示信息
+    private void initMyDevice() {
+        if (isConnected) {
+            deviceImg.setImageResource(R.drawable.watch);
+            deviceInfo.setText("智能手表");
+        } else {
+            deviceImg.setImageResource(R.drawable.connect);
+            deviceInfo.setText("连接设备");
+        }
+    }
+
+    //检查手表与手机是否连接
+    public void checkConnectState() {
+        Wearable.MessageApi.sendMessage(
+                mClient, "default", "CHECK_CONNECT_STATE", new byte[0]).setResultCallback(
+                new ResultCallback<MessageApi.SendMessageResult>() {
+                    @Override
+                    public void onResult(MessageApi.SendMessageResult sendMessageResult) {
+                        if (!sendMessageResult.getStatus().isSuccess()) {
+                            isConnected = false;
+                        } else {
+                            isConnected = true;
+                        }
+                    }
+                }
+        );
+    }
+
+    //监听手表发过来的信息
+    @Override
+    public void onMessageReceived(MessageEvent messageEvent) {
+        Log.e(TAG, "onMessageReceived: hava new msg");
+        if (messageEvent.getPath().equals("WEAR")) {
+            Message msg = new Message();
+            msg.what = 0x123;
+            msg.obj = new String(messageEvent.getData());
+            handler.sendMessage(msg);
+        }
+        if (messageEvent.getPath().equals("BATTERY_LEVEL")) {
+            Message msg = new Message();
+            msg.what = 0x125;
+            msg.arg1 = Integer.parseInt(new String(messageEvent.getData()));
+            handler.sendMessage(msg);
+        }
+    }
+
+    //API连接成功时回调
+    @Override
+    public void onConnected(Bundle bundle) {
+        Log.e(TAG, "onConnected: " + bundle);
+        Wearable.MessageApi.addListener(mClient, this);
+    }
+
+    //API连接中断时回调
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e(TAG, "onConnectionSuspended: " + i);
+    }
+
+    //API连接失败时调用
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        Log.e(TAG, "onConnectionFailed: " + connectionResult);
+    }
+
 }
