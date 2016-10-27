b9eac4e69548db123e537aabdf4c47b1f90fe3f7
zhangminvip <2107609379@qq.com>
2016-10-27 17:09:28 +0800

    添加闹钟响铃功能

---
+++ mobile/src/main/java/com/winorout/followme/personalCenter/AlarmActivity.java
@@ -0,0 +1,35 @@
+package com.winorout.followme.personalCenter;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+
+import com.winorout.followme.R;
+
+/**
+ * Created by minzhang on 2016/10/26.
+ */
+
+public class AlarmActivity extends Activity {
+    MediaPlayer alarmMusic;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        alarmMusic = MediaPlayer.create(this, R.raw.alarm);
+        alarmMusic.setLooping(true);
+
+        alarmMusic.start();
+        new AlertDialog.Builder(AlarmActivity.this).setTitle("闹钟")
+                .setMessage("闹钟响了")
+                .setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        alarmMusic.stop();
+                        AlarmActivity.this.finish();
+                    }
+                }).show();
+    }
+}
